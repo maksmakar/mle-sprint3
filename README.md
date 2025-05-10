09e695624c5b8dc2c7d75fca5b7aa80064dc977a
Maks <mmakarov@gmail.com>
2025-05-10 15:46:18 +0000

    Added files from the Topic_1

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-Репозиторий с файлами для прохождения третьего спринта.
+.
+├── app
+│   ├── churn_app.py
+│   ├── fast_api_handler.py
+│   ├── main.py
+│   └── model_loader.py
+├── models
+│   ├── catboost_churn_model.bin
+│   └── catboost_credit_model.bin
+├── README.md
+└── requirements.txt 
---
+++ app/model_loader.py
@@ -0,0 +1,26 @@
+# импортируйте необходимую библиотеку
+# ваш код здесь
+from fastapi import FastAPI
+from catboost import CatBoostClassifier
+
+def load_churn_model(model_path: str):
+    """Загружаем обученную модель оттока.
+    Args:
+        model_path (str): Путь до модели.
+    """
+    try:
+        model = CatBoostClassifier()
+        model.load_model(model_path)
+
+        print("Model loaded successfully")
+
+    except Exception as e:
+
+        print(f"Failed to load model: {e}")
+
+    return model
+
+if __name__ == "__main__":
+
+    model = load_churn_model('models/catboost_churn_model.bin')
+    print(f"Model parameter names: {model.feature_names_}") 
